9e09afc32efa7e7b8b2d64aadd1027944ec8a66b
jacob kleiman <jacobakleiman@gmail.com>
2020-02-27 21:24:12 -0800

    implemented routes, controllers, api v1 get and post users

---
+++ routes/api.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const ctrl = require('../controllers');
+
+// -------- User Routes
+router.get('/users', ctrl.users.index);
+// router.get('/users/:id', ctrl.users.show);
+router.post('/users', ctrl.users.create);
+// router.put('/users/:id', ctrl.users.update);  
+// router.delete('/users/:id', ctrl.users.destroy);
+  
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,40 @@
+// Require Statements
+const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('./models');
+
+const PORT = process.env.PORT || 4000;
+
+// Init Routes
+const routes = require('./routes'); // Routes Module
+
+// Init server
+const app = express();
+
+// ------------------- MIDDLEWARE
+
+// Init BodyParser
+app.use(bodyParser.json());
+
+// ------------------- VIEW ROUTES
+
+// app.use('/', routes.views);
+
+// ------------------- API ROUTES
+
+app.use('/api/v1', routes.api);
+
+// API ERROR 404
+app.use('/api/*', (req, res) => {
+  res.status(404).json({status: 404, error: "Error 404: Resource Not Found"});
+});
+
+// HTML Error 404
+app.use('*', (req, res) => {
+  res.send('<h2>Error 404: Page Not Found</h2>');
+});
+
+// ------------------ START SERVER
+
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
