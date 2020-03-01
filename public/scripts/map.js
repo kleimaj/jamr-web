4bd6a63341e1babc05e36607c6a744ac6d1de0ed
jacob kleiman <jacobakleiman@gmail.com>
2020-02-29 23:17:22 -0800

    Settings complete

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -2,31 +2,48 @@ console.log('Spice of Life');
 const default_coords = {lat: 37.791034, lng: -122.401605};
 // 37.791034, -122.401605
 // {lat: 37.78, lng: -122.44};
+let infoWindows = [];
 map = new google.maps.Map(document.getElementById('map'), {
     center: default_coords,
     zoom: 12
 });
 const icon = {
     url:'images/musician.png',
-    scaledSize: new google.maps.Size(20, 20)
+    scaledSize: new google.maps.Size(30, 30)
 }
 const iconOther = {
     url:'images/musicianOther.png',
-    scaledSize: new google.maps.Size(20, 20)
+    scaledSize: new google.maps.Size(30, 30)
 }
 let userMarker = new google.maps.Marker({
     position: default_coords, 
     map: map,
     icon: icon
 });
+let id = localStorage.getItem('_id');
+let thisArtist = localStorage.getItem('artistName');
+let thisInstrument = localStorage.getItem('instruments');
+let thisGenre = localStorage.getItem('genres');
+let thisBio = localStorage.getItem('bio');
+let infowindow = new google.maps.InfoWindow({
+    content: 
+    `<div class=viewContainer>
+        <h4>${thisArtist}</h4>
+        <p>${thisInstrument}</p>
+        <p>${thisGenre}</p>
+        <button class=viewButton>View</button>
+    </div>`
+  });
 userMarker.addListener('click', function() {
-    map.setZoom(15);
+    map.setZoom(12);
     map.setCenter(userMarker.getPosition());
+    infowindow.open(userMarker.get('map'), userMarker);
   });
 
 const getProfiles = () => {
 $('#map').css("display", "block");
 $('.prompt').css("display","none");
+
     $.ajax({
   
         // What kind of request
@@ -51,21 +68,22 @@ $('.prompt').css("display","none");
 
 function onSuccess(json) {
     console.log("Successfully retrieved profiles..."); 
-    console.log(json);
+    // console.log(json);
     for (let user of json) {
-        console.log(user.location);
+        // console.log(user.location);
         let lat = parseFloat(user.location[0]);
         let lng = parseFloat(user.location[1]);
-        console.log(lat,lng)
-        let userMarker = new google.maps.Marker({
+        // console.log(lat,lng)
+        let newMarker = new google.maps.Marker({
             position: {lat: lat, lng: lng}, 
             map: map,
             icon: iconOther
         });
-        userMarker.addListener('click', function() {
-            map.setZoom(15);
-            map.setCenter(userMarker.getPosition());
+        newMarker.addListener('click', function() {
+            map.setZoom(12);
+            map.setCenter(newMarker.getPosition());
           });
+        attachModals(newMarker, user);
     }
     // localStorage.setItem('_id', json._id);
 };
@@ -75,6 +93,30 @@ function onError(xhr, status, errorThrown) {
     console.log("Status: " + status);
     console.dir(xhr);
 };
+
+function attachModals(marker, userObject) {
+
+    console.log(userObject);
+    let infowindow = new google.maps.InfoWindow({
+        content: 
+        `<div class=viewContainer>
+            <h4>${userObject.artistName}</h4>
+            <p>${userObject.instruments}</p>
+            <p>${userObject.genres}</p>
+            <button class=viewButton>View</button>
+        </div>`
+      });
+
+      marker.addListener('click', function() {
+        if(infoWindows.length > 0){
+            console.log('here');
+            infoWindows[0].close();
+            infoWindows.splice(0,1);
+        }
+        infowindow.open(marker.get('map'), marker);
+        infoWindows.push(infowindow);
+      });
+}
 // getProfiles();
 $('#map').css("display", "none");
 $('.show').on('click', getProfiles);
---
+++ public/scripts/settings.js
@@ -0,0 +1,82 @@
+let id = localStorage.getItem('_id');
+let thisArtist = localStorage.getItem('artistName');
+let thisInstrument = localStorage.getItem('instruments');
+let thisGenre = localStorage.getItem('genres');
+let thisBio = localStorage.getItem('bio');
+
+function setDefaultInputs () {
+    $('.artistName')[0].value=thisArtist;
+    $('.bio')[0].value=thisBio;
+    let genreOptions = $('.genre').children('option');
+    console.log(thisGenre);
+    for (let i = 0; i < genreOptions.length; i++) {
+        console.log(genreOptions[i].textContent);
+        if (genreOptions[i].textContent.toLowerCase()===thisGenre) {
+            console.log(genreOptions[i].textContent)
+            genreOptions[i].selected=true;
+            break;
+        }
+    }
+    let instrOptions = $('.instru').children('option');
+    for (let i = 0; i < instrOptions.length; i++) {
+        if (instrOptions[i].textContent.toLowerCase()===thisInstrument) {
+            // console.log(instrOptions[i].textContent)
+            instrOptions[i].selected=true;
+            break;
+        }
+    }
+}
+
+$(".update").submit(function(event){
+    event.preventDefault();
+    // console.log('save');
+    //   Ajax
+    id = "5e5af74d746d0ca6fe861ec9"
+    let body = {
+        // _id: id,
+        artistName: $('.artistName').val(),
+        bio: $('.bio').val(),
+        instruments: $('.instru').val(),
+        genres: $('.genre').val(),
+        
+    };
+    console.log(body);
+    localStorage.setItem('artistName', body.artistName);
+    localStorage.setItem('instruments', body.instruments);
+    localStorage.setItem('genres', body.genres);
+    localStorage.setItem('bio', body.bio);
+    body = JSON.stringify(body);
+    $.ajax({
+  
+        // What kind of request
+        method: "PUT",
+    
+        // The URL for the request
+        url: `/api/v1/profile/${id}`,
+        
+        contentType: 'application/json',
+        // The data to send aka query parameters
+        data: body,
+        // data: $("form").serialize(),
+    
+        // Code to run if the request succeeds;
+        // the response is passed to the function
+        success: onSuccessUpdate,
+    
+        // Code to run if the request fails; the raw request and
+        // status codes are passed to the function
+        error: onError
+    });
+});
+
+function onSuccessUpdate(json) {
+    console.log('success');
+    console.log(json);
+}
+function onError(xhr, status, errorThrown) {
+    alert("Sorry, there was a problem!");
+    console.log("Error: " + errorThrown);
+    console.log("Status: " + status);
+    console.dir(xhr);
+};
+setDefaultInputs();
