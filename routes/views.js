cadea7c2bc2537c999798a7da1e9181e0a73b516
jesusquezada <2016quezadaj@gmail.com>
2020-02-29 23:42:22 -0800

    Installed socket.io and created chat demo

--- routes/views.js
+++ routes/views.js
@@ -38,6 +38,12 @@ res.sendFile('views/map/map.html', {
     });
 });
 
+router.get('/chat', (req, res) => {
+    res.sendFile('views/chat/chat.html', {
+            root: __dirname + '/../'
+        });
+    });
+
 router.get('/profiles/:id/settings', (req,res) => {
     res.sendFile('views/profiles/settings.html', {
         root: __dirname + '/../'
